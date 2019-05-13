14906b552124131103033346f4e9dabdc6235005
bdai09 <36967001+bdai09@users.noreply.github.com>
2019-05-12 22:35:57 -0400

    Update SecureData.h

--- C++_semester3/w9/SecureData.h
+++ C++_semester3/w9/SecureData.h
@@ -2,15 +2,7 @@
 // Workshop 9: Multi-Threading
 /SecureData.h
 ///////////////////////////////////////////////////////////
-// Name: Bo Dai
-// Seneca Student ID:132954173
-// Seneca email: bdai09@myseneca.ca
-// Date of completion: Nov 11, 2018
-//
-// I confirm that the content of this file is created by me,
-// with the exception of the parts provided to me by my professor
-///////////////////////////////////////////////////////////
-**********************************************************/
+
 #ifndef W9_SECUREDATA_H
 #define W9_SECUREDATA_H
 
