f2c599aaa9d14d559d94e0adbe8a200f22ab8559
bo dai <bdai9@myseneca.ca>
2019-05-14 16:20:49 -0400

    pre

--- Web_semester5/assignment-1/src/index.js
+++ Web_semester5/assignment-1/src/index.js
@@ -11,49 +11,49 @@ other source
 *
 ********************************************************************************/
 
-$(function(){
- //Handler for .ready() called.
-console.log("jQuery working"); 
+$(function() {
+    //Handler for .ready() called.
+    console.log('jQuery working');
 
-$("#teams-menu").on("click",function(event){
-    event.preventDefault();
-    $.get("https://agile-river-17555.herokuapp.com/teams")
-    .done(function(data){
-        $("#data").empty()
-        .append("<h3>Teams</h3>")
-        .append(JSON.stringify(data));
-        $("#data").html(prettyPrintJson.toHtml(data));
+    $('#teams-menu').on('click', function(event) {
+        event.preventDefault();
+        $.get('https://agile-river-17555.herokuapp.com/teams').done(function(data) {
+            $('#data')
+                .empty()
+                .append('<h3>Teams</h3>')
+                .append(JSON.stringify(data));
+            $('#data').html(prettyPrintJson.toHtml(data));
+        });
     });
-});
 
-$("#employees-menu").on("click",function(event){
-    event.preventDefault();
-    $.get("https://agile-river-17555.herokuapp.com/employees")
-    .done(function(data){
-        $("#data").empty()
-        .append("<h3>Employees</h3>")
-        .append(JSON.stringify(data));
-        $("#data").html(prettyPrintJson.toHtml(data));
+    $('#employees-menu').on('click', function(event) {
+        event.preventDefault();
+        $.get('https://agile-river-17555.herokuapp.com/employees').done(function(data) {
+            $('#data')
+                .empty()
+                .append('<h3>Employees</h3>')
+                .append(JSON.stringify(data));
+            $('#data').html(prettyPrintJson.toHtml(data));
+        });
     });
-});
-$("#projects-menu").on("click",function(event){
-    event.preventDefault();
-    $.get("https://agile-river-17555.herokuapp.com/projects")
-    .done(function(data){
-        $("#data").empty()
-        .append("<h3>Projects</h3>")
-        .append(JSON.stringify(data));
-        $("#data").html(prettyPrintJson.toHtml(data));
+    $('#projects-menu').on('click', function(event) {
+        event.preventDefault();
+        $.get('https://agile-river-17555.herokuapp.com/projects').done(function(data) {
+            $('#data')
+                .empty()
+                .append('<h3>Projects</h3>')
+                .append(JSON.stringify(data));
+            $('#data').html(prettyPrintJson.toHtml(data));
+        });
     });
-});
-$("#positions-menu").on("click",function(event){
-    event.preventDefault();
-    $.get("https://agile-river-17555.herokuapp.com/positions")
-    .done(function(data){
-        $("#data").empty()
-        .append("<h3>Positions</h3>")
-        .append(JSON.stringify(data));
-        $("#data").html(prettyPrintJson.toHtml(data));
+    $('#positions-menu').on('click', function(event) {
+        event.preventDefault();
+        $.get('https://agile-river-17555.herokuapp.com/positions').done(function(data) {
+            $('#data')
+                .empty()
+                .append('<h3>Positions</h3>')
+                .append(JSON.stringify(data));
+            $('#data').html(prettyPrintJson.toHtml(data));
+        });
     });
-});
 });
