3ec9d814b4f33acbdf53d6973a4b8c052815cf99
bdai09 <36967001+bdai09@users.noreply.github.com>
2019-07-06 21:11:52 -0400

    Create DatabaseHandler.java

---
+++ Android app/assign3/DatabaseHandler.java
@@ -0,0 +1,43 @@
+package com.example.bdai9.asgn3;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.List;
+
+public class DatabaseHandler extends SQLiteOpenHelper {
+    public DatabaseHandler(Context context){
+        super(context, "spinnerExample", null, 1);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db1){
+        db1.execSQL(
+                "CREATE TABLE items (id INTEGER PRIMARY KEY, name0 TEXT, name1 TEXT, name2 TEXT,name3 TEXT,name4 TEXT )"
+        );
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db2, int oldVersion, int newVersion){
+        db2.execSQL("DROP TABLE IF EXISTS items");
+        onCreate(db2);
+    }
+
+    public void insertItem(String itemA, String itemB,String itemC,String itemD,String itemE ){
+        SQLiteDatabase db3=this.getWritableDatabase();
+        ContentValues values=new ContentValues();
+        values.put("name0",itemA);
+        values.put("name1",itemB);
+        values.put("name2",itemC);
+        values.put("name3",itemD);
+        values.put("name4",itemE);
+        db3.insert("items", null, values);
+        db3.close();
+    }
+
+    public List<String> getAllItems0(){
+        List<String>
+    }
+
+}
