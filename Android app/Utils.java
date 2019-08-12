cce04c257b2d2930e3d76ee4662230d0d62552c6
bdai09 <36967001+bdai09@users.noreply.github.com>
2019-08-12 18:16:17 -0400

    Update Utils.java

--- Android app/Utils.java
+++ Android app/Utils.java
@@ -1,75 +1,72 @@
-public class FirstActivity extends AppCompatActivity {
-    ListView linearLayoutListView;
-    TextView url;
+public class MainActivity extends AppCompatActivity {
+    Button submit;
+    Spinner spinner1;
+    TextView spinnerTextView, date, changeDate;
+    boolean setSpinner;
+    String string2, currDate;
+    EditText magnitude;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_first);
-        Intent intent = getIntent();
-        String magnitude = intent.getStringExtra("magnitude");
-        String date2 = intent.getStringExtra("date");
-        String order = intent.getStringExtra("order");
-        String stringUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=" + date2 + "&minmagnitude=7.0&limit=" + magnitude + "&orderby=" + order;
-        QuakeAsyncTask task = new QuakeAsyncTask();
-        task.execute(stringUrl);
-    }
-    class QuakeAsyncTask extends AsyncTask<String, Void, List<String>> {
-        @Override
-        protected List<String> doInBackground(String... stringUrl) {
-            return Utils.fetchEarthquakeData(stringUrl[0]);
-        }
-        public void onPostExecute(List<String> postExecuteResult) {
-            CustomListAdapter arrayAdapter = new CustomListAdapter(FirstActivity.this, postExecuteResult);
-            linearLayoutListView = findViewById(R.id.listViewManyItemsID);
-            linearLayoutListView.setAdapter(arrayAdapter);
-            linearLayoutListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                    String lat = ((TextView) view.findViewById(R.id.textViewLat)).getText().toString();
-                    String log = ((TextView) view.findViewById(R.id.textViewLog)).getText().toString();
-                    String url = "https://www.openstreetmap.org/?mlat=" + lat + "&mlon=" + log + "#map=5/" + lat + "/" + log;
-                  Intent intent = new Intent(FirstActivity.this, WebActivity.class);
-                    intent.putExtra("url", url);
-                    startActivity(intent);
-                }
-            });
-        }
-    }
-}
-class CustomListAdapter extends ArrayAdapter<String> {
-    Activity context;
-    List<String> itemName1;
-    public CustomListAdapter(Activity activity, List<String> itemNameA) {
-        super(activity, R.layout.text_view1, itemNameA);
-        this.context = activity;
-        this.itemName1 = itemNameA;
-    }
-    public View getView(int position, View view, ViewGroup parent) {
-        LayoutInflater inflater = context.getLayoutInflater();
-        View rowView = inflater.inflate(R.layout.text_view1, null, true);
-        String earthInfo[] = itemName1.get(position).split("@@");
-        TextView title = rowView.findViewById(R.id.textViewInfo);
-        TextView date = rowView.findViewById(R.id.textViewDate);
-        TextView url = rowView.findViewById(R.id.textViewUrl);
-        TextView mag = rowView.findViewById(R.id.textViewMag);
-        TextView log = rowView.findViewById(R.id.textViewLog);
-        TextView lat = rowView.findViewById(R.id.textViewLat);
-        title.setText(earthInfo[0]);
-        date.setText(new Date(Long.parseLong(earthInfo[1])).toString());
-        url.setText(earthInfo[2]);
-        mag.setText(earthInfo[3]);
-        log.setText(earthInfo[4]);
-        lat.setText(earthInfo[5]);
-        double number = Double.parseDouble(mag.getText().toString());
-        System.out.print(number);
-        if (number > 7.4) {
-            title.setBackgroundColor(context.getResources().getColor(android.R.color.holo_red_light));
-            date.setBackgroundColor(context.getResources().getColor(android.R.color.holo_red_light));
-        }
-        if (number < 7.5) {
-            title.setBackgroundColor(context.getResources().getColor(android.R.color.holo_purple));
-            date.setBackgroundColor(context.getResources().getColor(android.R.color.holo_purple));
-        }
-        return rowView;
+        setContentView(R.layout.activity_main);
+        changeDate = findViewById(R.id.button1);
+        submit = findViewById(R.id.button2);
+        magnitude = findViewById(R.id.editText1);
+        date = findViewById(R.id.textView3);
+        currDate = new SimpleDateFormat("yyyy-M-d", Locale.getDefault()).format(new Date());
+        date.setText(currDate);
+        spinnerTextView = findViewById(R.id.spinnerTextView1);
+        setSpinner = false;
+        spinner1 = findViewById(R.id.spinner1);
+        final List<String> listItems = Arrays.asList("magnitude", "date");
+        
+        ArrayAdapter<String> dataAdapter1 = new ArrayAdapter<>(this, R.layout.spinner_item, listItems);
+        spinner1.setAdapter(dataAdapter1);
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                string2 = parent.getItemAtPosition(position).toString();
+                if (setSpinner)
+                    spinnerTextView.setText(string2);
+                spinnerTextView.setVisibility(View.GONE);
+                if (string2 == "date")
+                    string2 = "time";
+                spinnerTextView.setText(string2);
+                spinnerTextView.setVisibility(View.GONE);
+                setSpinner = true;
+                if (string2.length() > 1)
+                    spinner1.setVisibility(View.VISIBLE);
+            }
+           @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+        changeDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+                final DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                                date.setText(year + "-" + (month + 1) + "-" + day);
+                            }
+                        }, year, month, dayOfMonth);
+                datePickerDialog.show();
+            }
+        });
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FirstActivity.class);
+                intent.putExtra("magnitude", magnitude.getText().toString());
+                intent.putExtra("date", date.getText().toString());
+                intent.putExtra("order", spinnerTextView.getText().toString());
+                startActivity(intent);
+            }
+        });
     }
 }
