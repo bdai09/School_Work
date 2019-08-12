fb09b86e7dd4e2f3ad4a60c35bcb187282c75183
bdai09 <36967001+bdai09@users.noreply.github.com>
2019-08-12 17:51:36 -0400

    Create Utils.java

---
+++ Android app/Utils.java
@@ -0,0 +1,120 @@
+public final class Utils {
+    public static final String LOG_TAG = Utils.class.getSimpleName();
+
+    public static List<String> fetchEarthquakeData(String requestUrl) {
+        // Create URL object
+        URL url = createUrl(requestUrl);
+
+        // Perform HTTP request to the URL and receive a JSON response back
+        String jsonResponse = null;
+        try {
+            jsonResponse = makeHttpRequest(url);
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Error closing input stream", e);
+        }
+        return extractFeatureFormJason(jsonResponse);
+    }
+
+    private static URL createUrl(String stringUrl) {
+        URL url = null;
+        try {
+            url = new URL(stringUrl);
+        } catch (MalformedURLException e) {
+            Log.e(LOG_TAG, "Problem building the URL ", e);
+        }
+        return url;
+    }
+
+    private static String makeHttpRequest(URL url) throws IOException {
+        String jsonResponse = "";
+
+        // If the URL is null, then return early.
+        if (url == null) {
+            return jsonResponse;
+        }
+
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(10000 /* milliseconds */);
+            urlConnection.setConnectTimeout(15000 /* milliseconds */);
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            // If the request was successful (response code 200),
+            // then read the input stream and parse the response.
+            if (urlConnection.getResponseCode() == 200) {
+                inputStream = urlConnection.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+            } else {
+                Log.e(LOG_TAG, "Error response code: " + urlConnection.getResponseCode());
+            }
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Problem retrieving the earthquake JSON results.", e);
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (inputStream != null) {
+                // Closing the input stream could throw an IOException, which is why
+                // the makeHttpRequest(URL url) method signature specifies than an IOException
+                // could be thrown.
+                inputStream.close();
+            }
+        }
+        return jsonResponse;
+    }
+
+    private static String readFromStream(InputStream inputStream) throws IOException {
+        StringBuilder output = new StringBuilder();
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null) {
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+        return output.toString();
+    }
+
+    private static List<String> extractFeatureFormJason(String earthquakeJSON) {
+        if (TextUtils.isEmpty(earthquakeJSON)) {
+            return null;
+        }
+
+
+        List<String> quakeList = new ArrayList<>();
+
+        try {
+
+
+            JSONObject baseJsonResponse = new JSONObject(earthquakeJSON);
+
+
+            JSONArray earthquakeArray = baseJsonResponse.getJSONArray("features");
+
+
+            for (int i = 0; i < earthquakeArray.length(); i++) {
+
+
+                JSONObject currentEarthquake = earthquakeArray.getJSONObject(i);
+
+
+                JSONObject properties = currentEarthquake.getJSONObject("properties");
+
+                String title = properties.getString("title");
+                String time = properties.getString("time");
+                String url = properties.getString("url");
+                quakeList.add(title + " " + time + " " + url);
+
+            }
+            return quakeList;
+        } catch (JSONException e) {
+            Log.e("QueryUtils", "Problem parsing the earthquake JSON results", e);
+        }
+        return null;
+    }
+}
